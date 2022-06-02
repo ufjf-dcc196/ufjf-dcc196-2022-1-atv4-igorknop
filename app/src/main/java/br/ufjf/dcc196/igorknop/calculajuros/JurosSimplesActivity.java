d3ee25c06f9119083d722d0442686041ae0cdd73
Igor Knop <igor.knop@ufjf.br>
2022-06-01 21:55:47 -0300

    Adiciona evento de navegação para simples

--- app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/JurosSimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/JurosSimplesActivity.java
@@ -3,6 +3,7 @@ package br.ufjf.dcc196.igorknop.calculajuros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class JurosSimplesActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class JurosSimplesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juros_simples);
+    }
+
+    public void retornarClick(View view){
+        finish();
+    }
+
+    public void calcularClick(View view ){
+
     }
 }
