c886869d6e953e7648de206233267763b2dad089
Igor Knop <igor.knop@ufjf.br>
2022-06-01 22:08:19 -0300

    Adiciona passagem de dados entre activities

--- app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/JurosSimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/JurosSimplesActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class JurosSimplesActivity extends AppCompatActivity {
-
+    TextView textViewValorPresente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juros_simples);
+        textViewValorPresente = findViewById(R.id.textViewValorPresente);
+        Bundle extras = getIntent().getExtras();
+        Double valorPresente = extras.getDouble("valorPresente");
+        textViewValorPresente.setText(valorPresente.toString());
     }
 
     public void retornarClick(View view){
--- app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/MainActivity.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextValorPresente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextValorPresente = findViewById(R.id.editTextValorPresente);
     }
 
-    public void abreJurosSimples(View view){
+    public void jurosSimplesClick(View view){
+        Double valorPresente = Double.parseDouble(editTextValorPresente.getText().toString());
+
         Intent intent = new Intent(MainActivity.this, JurosSimplesActivity.class);
+        intent.putExtra("valorPresente", valorPresente);
         startActivity(intent);
     }
 }
