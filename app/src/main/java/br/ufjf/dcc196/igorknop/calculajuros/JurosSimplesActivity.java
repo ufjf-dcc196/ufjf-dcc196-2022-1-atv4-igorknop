9168bf18bcd66b6685a6efe434f47bba9e0f6bf6
Igor Knop <igor.knop@ufjf.br>
2022-06-01 21:31:31 -0300

    Cria activity para juros simples

---
+++ app/src/main/java/br/ufjf/dcc196/igorknop/calculajuros/JurosSimplesActivity.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.igorknop.calculajuros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JurosSimplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juros_simples);
+    }
+}
